556d9eabc21a486fbe96c02fe3ab9dd1cd6895bd
razin.ahmed <raheenahmee@gmail.com>
2024-05-04 04:01:29 +0500

    react firts commit

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,10 +1,15 @@
 
+import chai from "./chai"
+
 function App() {
 
-  return (
-    
-       
-<h1> hello world | Razin Ahmed</h1>  )
+ return (
+  <>
+  <h2>chai aur react</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ratione vel modi fugiat porro labore eligendi consectetur voluptates laboriosam temporibus, fuga non pariatur illum repellat et optio molestiae reprehenderit eveniet!</p>
+  </>
+ )
 }
 
 export default App
+
