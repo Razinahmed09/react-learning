56b292151168125fd0eec5eca607eb44899e4c05
razin.ahmed <raheenahmee@gmail.com>
2024-05-11 04:11:23 +0500

    create another project in react project name cards

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
-
+import Card from './component/cards';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className=" text-white !bg-black text-5xl font-bold underline">
+      <h1 className=" text-white !bg-black  rounded-xl text-5xl font-bold">
       Hello world!
     </h1>
+    <Card />
+
     </>
   )
 }
 
 export default App;
+
+
+// props reamaining
