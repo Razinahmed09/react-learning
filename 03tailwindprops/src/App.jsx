3d18d180e7ddb9a967f824bec2ad39375f9cb084
Razin Ahmed <raheenahmee@gmail.com>
2024-05-13 13:06:28 +0500

    chnages in cards

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -3,19 +3,26 @@ import Card from './component/cards';
 
 function App() {
   // const [count, setCount] = useState(0)
+ let myObj = {
+  username : "razin",
+  age : 21
+ }
 
+let newArr = [1,2,3]
+ 
   return (
     <>
       <h1 className=" text-white !bg-black  rounded-xl text-5xl font-bold">
-      Hello world!
+      Tailwind test
     </h1>
-    <Card />
+    <Card username="chaiourcode" someObje={myObj} btnText={"click me"}/>
+    <Card someObje={newArr} username="click me" btnText={"visit me"}/>
 
     </>
   )
 }
 
-export default App;
+export default App; 
 
 
 // props reamaining
