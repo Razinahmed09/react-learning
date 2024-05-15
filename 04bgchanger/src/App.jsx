624043828647b63895501beea41a838d40d697ff
Razin Ahmed <raheenahmee@gmail.com>
2024-05-15 18:45:58 +0500

    some changes

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -6,8 +6,20 @@ function App() {
 
   return (
     <div className="h-screen w-full duration-200 " style={{backgroundColor:color}}>
+      <div className="fixed flex flex-wrap bottom-12 inset-x-0 px-2 justify-center gap-2">
+        <div className="flex flex-wrap justify-center bg-white shadow-lg rounded-2xl p-2"><button className='bg-red-400 rounded-full py-2 px-2'>text</button></div>
+        <div className="flex flex-wrap justify-center bg-white shadow-lg rounded-2xl p-2"><button className='bg-red-400 rounded-full py-2 px-2'>text</button></div>
+        <div className="flex flex-wrap justify-center bg-white shadow-lg rounded-2xl p-2"><button className='bg-red-400 rounded-full py-2 px-2'>text</button></div>
+        <div className="flex flex-wrap justify-center bg-white shadow-lg rounded-2xl p-2"><button className='bg-red-400 rounded-full py-2 px-2'>text</button></div>
+        <div className="flex flex-wrap justify-center bg-white shadow-lg rounded-2xl p-2"><button className='bg-red-400 rounded-full py-2 px-2'>text</button></div>
+      </div>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
