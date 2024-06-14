07984f11b98872f2d7483787d7b7f88fbd33c301
Razin Ahmed <raheenahmee@gmail.com>
2024-06-14 04:03:30 -0700

    in process

--- 05cars-project/src/App.jsx
+++ 05cars-project/src/App.jsx
@@ -6,27 +6,40 @@ function App() {
 
   return (
     <>
-            
+
       <div>
         <nav className='flex justify-around py-2'>
           <div>
-            <img 
-            src="/img/brand_logo.png"
-            alt="" />
+            <img
+              src="/img/brand_logo.png"
+              alt="" />
           </div>
-          
-            <ul className='flex gap-3'> 
-              <li href="#">home</li>
-              <li href="#">About</li>
-              <li href="#">Gallery </li>
-              <li href="#">Contact Us</li>
-            </ul>
-          <button className='bg-red-600 text-white rounded-full py-2 px-4'>
+
+          <ul className='flex gap-3'>
+            <li href="#">home</li>
+            <li href="#">About</li>
+            <li href="#">Gallery </li>
+            <li href="#">Contact Us</li>
+          </ul>
+          <button
+            className='bg-red-600 text-white rounded-full py-2 px-4'
+          >
             login
           </button>
         </nav>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+                
+            </div>
+            <div className="col-6">
+
+            </div>
+          </div>
+        </div>
       </div>
-        
+
     </>
   )
 }
