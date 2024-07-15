37d3d03593dec78b4e4d8cd9cdd1f6805981907f
Razin Ahmed <raheenahmee@gmail.com>
2024-07-15 07:07:31 -0700

    cretae

---
+++ 07contact-us/src/component/nav.jsx
@@ -0,0 +1,22 @@
+const Nav = () => {
+    return (
+        <>
+        <nav>
+            <ul>
+                <li>
+                    <a 
+                    href="#"
+                    >
+                    <img
+                     src="../../public/img/Header.png"
+                    alt="logo-img"
+                     className="w-auto h-10 " />
+                    </a>
+                </li>
+                <li></li>
+            </ul>
+        </nav>
+        </>
+    ); 
+}
+export default Nav 
