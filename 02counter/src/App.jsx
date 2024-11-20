9b4d04776cae942d5d5d2eed86ad2b19a82ad946
Razin Ahmed <raheenahmee@gmail.com>
2024-11-20 15:05:06 +0500

    create counter in react

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,41 +2,73 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [counter, setcounter] = useState(15);
+  // const [counter, setcounter] = useState(15);
 
+
+  
   // const addvalue = () => {
-  //   if (counter < 20) {
+  //   if (counter < 20){
   //     setcounter(counter + 1);
   //   }
+  //   // console.log("Im Razin Ahmed", Math.random());
   // }
-
-  // const removeValue = () => {
-  //   if (counter > 0) {
-  //     setcounter(counter - 1);
+  //   const removeValue = () => {
+  //     if (counter > 0) {
+  //       setcounter(counter - 1);
+  //     }
   //   }
-  const addvalue = () => {
-    if (counter < 20){
-      setcounter(counter + 1);
-    }
-  }
-    const removeValue = () => {
-      if (counter > 0) {
-        setcounter(counter - 1);
-      }
-    }
  
   
+  // return (
+  //   <>
+  //   <h1>chai our react</h1>
+  //   <h2>counter value</h2>
+
+  //   <button onClick={addvalue}>add value {counter}</button>
+  //   <br />
+  //   <button onClick={removeValue}>remove value {counter}</button>
+  //   <footer>remove value {counter}</footer>
+  //   </>
+  // )
+
+ 
+  let [counter, setcounter] = useState(15)
+
+  // let counter = 0
+
+  const addvalue = () => {
+    console.log("value added:", counter)
+    if (counter < 20){
+      counter = counter + 1;
+      setcounter(counter);
+  }
+  }
+  const removeValue = () => {
+    console.log("value remove", counter)
+    if (counter > 0) {
+      counter = counter - 1;
+      setcounter(counter);
+    }
+  }
   return (
     <>
     <h1>chai our react</h1>
-    <h2>counter value</h2>
-
-    <button onClick={addvalue}>add value {counter}</button>
+    <h2>counter value: {counter}</h2>
+    <div>
+    <button
+    onClick={addvalue}
+    >Add Value:{counter}</button>
     <br />
-    <button onClick={removeValue}>remove value {counter}</button>
-    <footer>remove value {counter}</footer>
+    <button
+    onClick={removeValue}
+    >Remove Value:{counter}</button>
+    </div>
+    <footer >
+      footer value is : {counter}
+    </footer>
     </>
   )
+
 }
 
 export default App
