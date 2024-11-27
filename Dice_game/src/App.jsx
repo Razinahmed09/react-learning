a2dad70219a94e5746118be7867c3323190af90d
Razin Ahmed <raheenahmee@gmail.com>
2024-11-27 14:52:52 +0500

    create new project dice_game in react

---
+++ Dice_game/src/App.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+
+const Button = styled.button`
+background-color:black;
+color: white;
+// border: none;
+padding: 10px 20px;
+`;
+
+function App() {
+
+
+  return (
+    <>
+   <Button>This is Button</Button>
+  
+    </>
+  )
+}
+
+export default App
