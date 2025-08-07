49ff7884ae3be448bec10988e4ee050660bd409e
Razinahmed09 <141953648+Razinahmed09@users.noreply.github.com>
2025-08-07 13:41:45 +0500

    some changes

--- installation method/react-learning/src/App.jsx
+++ installation method/react-learning/src/App.jsx
@@ -1,13 +1,11 @@
 import Chai from "./chai"
 
 function App() {
- const username = "chai or code"
+ //const username = "chai or code"
 
  return (
   <>
   <Chai/>
-  <h1>chai or react {username}</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, consectetur.</p>
   </>
  )
 }
--- installation method/react-learning/src/main.jsx
+++ installation method/react-learning/src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Chai from './chai.jsx'
 import App from './App.jsx'
 
-function Myapp(){
-    return (
-        <div>
-            <h1>Custom app !</h1>
-        </div>
-    )
-}
+// function Myapp(){
+//     return (
+//         <div>
+//             <h1>Custom app !</h1>
+//         </div>
+//     )
+// }
 
 // const reactElement = {
 //     type: 'a',
@@ -23,20 +24,22 @@ function Myapp(){
 //     <a href="https//google.com" tager="_blank">visit google</a>
 // )
 
-const anotherUser = ("chai or react")
+// const anotherUser = ("chai or react")
 
-const ReactElement = React.createElement(
-    'a',
-    { href: 'https//google.com', target: '_blank'}, 
-    'click me to see google',
-    anotherUser
-)
+// const ReactElement = React.createElement(
+//     'a',
+//     { href: 'https//google.com', target: '_blank'}, 
+//     'click me to see google',
+//     anotherUser
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     
     // anotherElement
-    ReactElement,
+    // ReactElement,
     // anotherElement,
-        App
+        <App />,
+        // <Myapp/>,
+        <Chai/>
 )
